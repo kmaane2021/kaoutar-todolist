70e611b602d4a3108f722c4b1eddb153c9b27749
kaoutar <kmaane@hotmail.com>
2021-03-07 17:26:58 -0500

    Updates

---
+++ precache-manifest.2c087598ea6e016b6d414f474ab1e85c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d0cd1824a251b3bd3e4058b93c6fd2",
+    "url": "/kaoutar-todolist/index.html"
+  },
+  {
+    "revision": "ab2b6a94425c7459ece3",
+    "url": "/kaoutar-todolist/static/css/main.40c10ff6.chunk.css"
+  },
+  {
+    "revision": "ec81e0c0fa3a420c376b",
+    "url": "/kaoutar-todolist/static/js/2.e6b7563b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/kaoutar-todolist/static/js/2.e6b7563b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2b6a94425c7459ece3",
+    "url": "/kaoutar-todolist/static/js/main.068552ce.chunk.js"
+  },
+  {
+    "revision": "b9a62d51483ebdaad101",
+    "url": "/kaoutar-todolist/static/js/runtime-main.8aa30e46.js"
+  },
+  {
+    "revision": "4f646371b2a2501c7acd9110625993ca",
+    "url": "/kaoutar-todolist/static/media/background.4f646371.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/kaoutar-todolist/precache-manifest.2c087598ea6e016b6d414f474ab1e85c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kaoutar-todolist/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
